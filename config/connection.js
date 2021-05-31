a6c52c0a766c87c130fa1085a258e24b53dd68c5
nadhiw <nwoliye@gmail.com>
2021-05-31 11:28:30 -0500

    create the connection.js folder & the create connection

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql")
+const util = require("util")
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gadaaw@8824',
+    database: 'employees'
+})
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
